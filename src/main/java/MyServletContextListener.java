23ce429629e53162e003bb8b705732ce91429d0b
RagingPython <SHOOTni1>
2017-09-14 19:13:35 +0300

    IT WORKS

---
+++ src/main/java/MyServletContextListener.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+class MyThreadClass extends Thread{
+    public void run(){}
+    public void doShutdown(){}
+}
+
+public class MyServletContextListener implements ServletContextListener{
+    private MyThreadClass myThread = null;
+
+    public void contextInitialized(ServletContextEvent sce) {
+        if ((myThread == null) || (!myThread.isAlive())) {
+            myThread = new MyThreadClass();
+            myThread.start();
+        }
+    }
+
+    public void contextDestroyed(ServletContextEvent sce){
+        try {
+            myThread.doShutdown();
+            myThread.interrupt();
+        } catch (Exception ex) {
+        }
+    }
+}
